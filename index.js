d4eb015c657784c818a20279992fedd053afa3ac
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-18 16:55:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code here!
 
 document.getElementById("main").remove()
-let newHeader = document.getElementById
+let newHeader = document.createElement("h1")
+newHeader.innerHTML = "sweet message"
